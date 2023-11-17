1e9f79f252db50fe45e9e920498e87863e0b2e5e
Jason Wilkes <notarealdeveloper@gmail.com>
2023-11-17 16:48:34 +0000

    Version 0.0.4

--- src/assure/bytes.py
+++ src/assure/bytes.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 
 __all__ = [
-    'read',
     'bytes',
 ]
 
 import os
 import builtins
 
-def read(arg):
-    if hasattr(arg, 'read'):
-        return arg.read()
-    elif os.path.exists(arg):
-        return open(arg, 'rb').read()
-    else:
-        return arg
-
 def bytes(arg):
-    # TODO: don't call read in here, add more functions and fix the users
-    arg = read(arg)
+    if isinstance(arg, builtins.bytes):
+        return arg
+    if hasattr(arg, 'read'):
+        arg = arg.read()
+    elif os.path.exists(arg):
+        arg = open(arg, 'rb').read()
     if isinstance(arg, str):
         arg = arg.encode()
     if isinstance(arg, builtins.bytes):
